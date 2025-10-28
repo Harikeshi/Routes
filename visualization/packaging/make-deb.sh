740e0e16b4f9ee89439356b1d3424bf4ac765666
Щеголихин Сергей Николаевич <ShchegolikhinSN@01050-1.ru>
2025-10-28 14:12:41 +0300

    Редактирование make-deb.

--- visualization/packaging/make-deb.sh
+++ visualization/packaging/make-deb.sh
@@ -2,50 +2,28 @@
 set -euo pipefail
 
 # --- Параметры пакета ---
-
 PKG_NAME="visualization"
-PKG_VER="1.0.0"
+PKG_VER="1.3.0"
 ARCH="$(dpkg --print-architecture)"                   # amd64, arm64 и т.п.
 TRIPLET="$(dpkg-architecture -qDEB_HOST_MULTIARCH)"   # x86_64-linux-gnu и т.п.
 
 # --- Пути проекта ---
-
-ROOT_DIR="$(cd "$(dirname "$0")/.." && pwd)"
-BUILD_DIR="$ROOT_DIR/build/visualization"             # где лежит собранный бинарник
-BIN_NAME="visualization"                        # имя бинарника
-LIBD_DIR="$ROOT_DIR/../libs"               #! ваши готовые библиотеки
+ROOT_DIR="$(cd "$(dirname "$0")/../.." && pwd)"
+BUILD_DIR="$ROOT_DIR/cmake-build-release-astra/visualization"             # где лежит собранный бинарник
+echo $BUILD_DIR
+BIN_NAME="Visualization"
 
 # --- Директория сборки пакета ---
-
-PKG_DIR="$ROOT_DIR/pkg-${PKG_NAME}_${PKG_VER}"
+PKG_DIR="$HOME/tmp/pkg-${PKG_NAME}_${PKG_VER}"
 rm -rf "$PKG_DIR"
 install -d "$PKG_DIR/DEBIAN"
 install -d "$PKG_DIR/usr/bin"
 install -d "$PKG_DIR/usr/lib/$TRIPLET"
 
-# --- Копируем бинарь и библиотеки ---
-
+# --- Копируем бинарь---
 install -m755 "$BUILD_DIR/$BIN_NAME" "$PKG_DIR/usr/bin/$BIN_NAME"
 
-# Важно сохранить симлинки версий, если они есть
-
-# Копируем все *.so и *.so.* как есть
-
-shopt -s nullglob
-for f in "$LIBD_DIR"/*.so "$LIBD_DIR"/*.so.*; do
-
-# cp -a сохраняет симлинки и права; при необходимости поправим права ниже
-
-cp -a "$f" "$PKG_DIR/usr/lib/$TRIPLET/"
-done
-shopt -u nullglob
-
-# Приводим права для реальных файлов .so (симлинки не трогаем)
-
-find "$PKG_DIR/usr/lib/$TRIPLET" -type f -name "*.so*" -exec chmod 755 {} +
-
 # --- control ---
-
 cat > "$PKG_DIR/DEBIAN/control" <<EOF
 Package: $PKG_NAME
 Version: $PKG_VER
@@ -58,7 +36,6 @@ Description: Приложение визуализации.
 EOF
 
 # --- postinst/postrm (ldconfig) ---
-
 cat > "$PKG_DIR/DEBIAN/postinst" <<'EOF'
 #!/bin/sh
 set -e
@@ -76,7 +53,6 @@ EOF
 chmod 0755 "$PKG_DIR/DEBIAN/postrm"
 
 # --- Сборка .deb ---
-
 OUT_DEB="${PKG_NAME}_${PKG_VER}_${ARCH}.deb"
 dpkg-deb --build "$PKG_DIR" "$ROOT_DIR/$OUT_DEB"
 
